3e5c621394dba5243f2df5a30d41f82c698fe869
salah <salah.yousef.muhammad@gmail.com>
2018-12-30 13:23:56 +0200

    upgrade the functionality and use the angular service pattern

---
+++ src/app/services/data.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { Question } from "../models/Question";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  questions:Question[];
+
+  constructor() {
+    this.questions = [
+      {
+        text:"what is your name",
+        answer:"my name is salah",
+        hide:true
+      },
+      {
+        text:"what is your age",
+        answer:"my age is 25",
+        hide:true
+      },
+      {
+        text:"what is your job",
+        answer:"my job is frontend developer",
+        hide:true
+      },
+    ];
+
+   }
+
+   getQuestions() {
+     return this.questions;
+   }
+}
